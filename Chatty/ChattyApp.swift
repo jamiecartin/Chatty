3a2e0781ad8b1ad13db065b96d9313613985ec82
jamiecartin <jamie.cartin@icloud.com>
2023-03-15 20:55:25 -0400

    Initial Commit

---
+++ Chatty/ChattyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattyApp.swift
+//  Chatty
+//
+//  Created by Jamie Cartin on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ChattyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
